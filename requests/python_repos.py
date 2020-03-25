23d5a6d3f30a565c68912a59fd2630be5bdafb02
leif li <leif@leifdeMacBook-Pro.local>
2020-03-25 10:59:01 +0800

    request python pro info inhub

---
+++ requests/python_repos.py
@@ -0,0 +1,44 @@
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("status code:", r.status_code)
+response_dict = r.json()
+print(response_dict.keys())
+print(response_dict['total_count'])
+
+repo_dicts = response_dict['items']
+print(len(repo_dicts))
+
+repo_dict = repo_dicts[0]
+print(repo_dict['owner']['login'])
+print("\nkeys:",len(repo_dict))
+for key in sorted(repo_dict.keys()):
+	print(key)
+
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+	names.append(repo_dict['name'])
+	plot_dict = {
+	'value': repo_dict['stargazers_count'],
+	'label':repo_dict['description'],
+	'xlink': repo_dict['html_url'],
+	}
+	plot_dicts.append(plot_dict)
+my_style = LS('#333366', base_style = LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label =15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config,style=my_style)
+chart.title = 'Most-starred python projects on github'
+chart.x_labels = names
+chart.add('',plot_dicts)
+chart.render_to_file('python_repos.svg')
